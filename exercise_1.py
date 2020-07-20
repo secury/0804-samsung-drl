0f64c20b8d5e1df5880b2c1dbbf6897d06482781
Jongmin Lee <starjongmin@gmail.com>
2020-07-20 14:31:17 +0900

    update code

--- exercise_1.py
+++ exercise_1.py
@@ -4,126 +4,172 @@ from collections import deque
 
 import gym
 import numpy as np
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.optimizers import Adam
+import tensorflow as tf
+from tensorflow.python.keras.layers import Dense, Input, Layer
 
 np.set_printoptions(precision=3, suppress=True, threshold=10000, linewidth=250)
 
 
-class DQNAgent:
+class ReplayBuffer:
 
-    def __init__(self, state_dim, action_size, gamma=0.99):
-        self.state_dim = state_dim
-        self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = gamma
+    def __init__(self, memory_size=10000):
+        self.memory = deque(maxlen=memory_size)
 
+    def __len__(self):
+        return len(self.memory)
+
+    def add(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = [], [], [], [], []
+        for _ in range(batch_size):
+            idx = np.random.randint(len(self.memory))
+            state, action, reward, next_state, done = self.memory[idx]
+            state_batch.append(state)
+            action_batch.append(action)
+            reward_batch.append(reward)
+            next_state_batch.append(next_state)
+            done_batch.append(done)
+        return np.array(state_batch), np.array(action_batch), np.array(reward_batch)[:, None], np.array(next_state_batch), np.array(done_batch)[:, None]
+
+
+class QNetwork(Layer):
+
+    def __init__(self, state_dim, num_actions):
+        super(QNetwork, self).__init__()
+        self.l1 = Dense(64, activation='relu')
+        self.l2 = Dense(64, activation='relu')
+        self.out = Dense(num_actions)
+
+    def call(self, inputs, **kwargs):
+        """
+        :param state: [batch_size, state_dim] 크기의 numpy array
+        :return: [batch_size, num_actions] 크기의 numpy array
+        """
+        state, = inputs
+        h = self.l1(state)
+        h = self.l2(h)
+        q_values = self.out(h)  # [batch_size, num_actions]
+        return q_values
+
+
+class DQN:
+
+    def __init__(self, env, discount):
+        self.env = env
+        self.state_dim = env.observation_space.shape[0]
+        self.num_actions = env.action_space.n
+        self.discount = discount
+
+        # Hyper parameters
         self.batch_size = 64
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.1
-        self.epsilon_decay = 0.995
+        self.epsilon_min = 0.05
+        self.epsilon_decay = 0.997
 
-        self.q_model = self._build_model()
-        self.q_model.compile(loss='mse', optimizer=Adam(lr=0.001))
-        self.q_model.predict_one = lambda x: self.q_model.predict(np.array([x]))[0]
-        self.target_q_model = self._build_model()
-        self.target_q_model.predict_one = lambda x: self.target_q_model.predict(np.array([x]))[0]
+        # TF session / placeholders
+        self.sess = tf.keras.backend.get_session()
+        self.state_ph = Input(self.state_dim, name='state', dtype=tf.float32)  # [batch_size, state_dim]
+        self.action_ph = Input((), name='action', dtype=tf.int32)  # [batch_size]
+        self.reward_ph = Input(1, name='reward', dtype=tf.float32)  # [batch_size, 1]
+        self.next_state_ph = Input(self.state_dim, name='next_state', dtype=tf.float32)  # [batch_size, state_dim]
+        self.done_ph = Input(1, name='done', dtype=tf.float32)  # [batch_size, 1]
 
+        # Q-Network / target Q-network
+        self.q = QNetwork(self.state_dim, self.num_actions)
+        self.q_target = QNetwork(self.state_dim, self.num_actions)
+
+        # Construct computation graph (loss function)
+        self.q_values = self.q([self.state_ph])  # [batch_size, num_actions]
+        action_mask = tf.one_hot(self.action_ph, self.num_actions) # [batch_size, num_actions]
+        q_sa = tf.reduce_sum(self.q_values * action_mask, axis=1, keepdims=True)  # [batch_size, 1]
+
+        next_q_values = self.q_target([self.next_state_ph])  # [batch_size, num_actions]
+        next_q = tf.stop_gradient(tf.reduce_max(next_q_values, axis=1, keepdims=True))  # [batch_size, 1]
+        #################################
+        # TODO: implement here!
+        y = 0
+        #################################
+
+        loss = tf.reduce_mean((q_sa - y) ** 2)
+
+        # Loss minimizer
+        optimizer = tf.train.AdamOptimizer(0.001)
+        self.train_op = optimizer.minimize(loss, var_list=self.q.trainable_variables)
+
+        # Initialize variables
+        self.sess.run(tf.variables_initializer(optimizer.variables()))
         self.update_target_q_weights()  # target Q network 의 파라미터를 Q-newtork 에서 복사
-
-    def _build_model(self):
-        model = Sequential([
-            Dense(64, input_dim=self.state_dim, activation='relu'),
-            Dense(64, activation='relu'),
-            Dense(self.action_size, activation='linear')
-        ])
-        return model
 
     def update_target_q_weights(self):
         #################################
-        # TODO:
+        # TODO: implement here!
         pass
         #################################
 
-    def act(self, state):
-        # epsilon-greedy policy
+    def decay_epsilon(self):
+        self.epsilon = np.max([self.epsilon * self.epsilon_decay, self.epsilon_min])
+
+    def epsilon_greedy(self, state):
         if np.random.rand() <= self.epsilon:
-            return np.random.randint(0, self.action_size)
+            return np.random.randint(0, self.num_actions)
         else:
-            q_values = self.q_model.predict_one(state)
+            q_values = self.sess.run(self.q_values, feed_dict={self.state_ph: np.array([state])})[0]
             return np.argmax(q_values)
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
     def train(self):
-        if len(agent.memory) < self.batch_size:
-            return
-        mini_batch = random.sample(self.memory, self.batch_size)
-        input_state_batch, target_q_values_batch = [], []
+        replay_buffer = ReplayBuffer()
 
-        for state, action, reward, next_state, done in mini_batch:
-            q_values = self.q_model.predict_one(state)
+        for episode in range(5000):
+            state = self.env.reset()
 
-            #################################
-            # TODO:
-            if done:
-                q_values[action] = 0
-            else:
-                q_values[action] = 0
-            ##################################
+            episode_reward = 0.
+            for t in range(10000):
+                # action 선택 및 환경에 실행
+                action = self.epsilon_greedy(state)
+                next_state, reward, terminal, info = self.env.step(action)
+                episode_reward += reward
 
-            input_state_batch.append(state)
-            target_q_values_batch.append(q_values)
+                # Replay buffer 에 (s,a,r,s') 저장
+                done = terminal and info.get('TimeLimit.truncated') != True
+                replay_buffer.add(state, action, reward, next_state, done)
 
-        # Q-network 학습
-        self.q_model.fit(np.array(input_state_batch), np.array(target_q_values_batch), batch_size=self.batch_size, epochs=1)
+                # 30 에피소드마다 visualization
+                if episode % 30 == 0 or episode > 3000:
+                    q_values = self.sess.run(self.q_values, feed_dict={self.state_ph: np.array([state])})[0]
+                    print("[epi=%4d,t=%4d] state=%4s / action=%s / reward=%7.4f / next_state=%4s / Q[s]=%s" % (episode, t, state, action, reward, next_state, q_values))
+                    env.render()
+                    time.sleep(0.01)
 
-    def update_epsilon(self):
-        self.epsilon = np.max([self.epsilon * self.epsilon_decay, self.epsilon_min])
+                # Replay buffer에서 {(s,a,r,s')} 미니 배치 샘플 후 gradient descent 한 번 수행
+                if len(replay_buffer) >= self.batch_size:
+                    state_batch, action_batch, reward_batch, next_state_batch, done_batch = replay_buffer.sample(self.batch_size)
+                    self.sess.run(self.train_op, feed_dict={
+                        self.state_ph: state_batch,
+                        self.action_ph: action_batch,
+                        self.reward_ph: reward_batch,
+                        self.next_state_ph: next_state_batch,
+                        self.done_ph: done_batch
+                    })
+
+                if terminal:
+                    break
+                state = next_state
+
+            # 에피소드가 끝날 때마다 epsilon 을 점차 낮춘다.
+            self.decay_epsilon()
+
+            # 에피소드마다 10번마다 target network 의 파라미터를 현재 Q-network 파라미터로 갱신해준다.
+            if episode % 10 == 0:
+                self.update_target_q_weights()
+
+            print('[%4d] Episode reward=%.4f / epsilon=%f' % (episode, episode_reward, self.epsilon))
 
 
 """ Load environment """
 env_name = 'CartPole-v0'
-# env_name = 'MyPendulum-v0'
 
 env = gym.make(env_name)
-env.T = env.R = None
-
-state_dim = env.observation_space.shape[0]
-action_size = env.action_space.n
-
-agent = DQNAgent(state_dim, action_size, gamma=0.99)
-
-for episode in range(5000):
-    state = env.reset()
-
-    episode_reward = 0.
-    for t in range(10000):
-        action = agent.act(state)
-        next_state, reward, done, info = env.step(action)
-
-        # Replay buffer 에 (s,a,r,s') 저장
-        agent.remember(state, action, reward, next_state, done)
-
-        episode_reward += reward
-        print("[epi=%4d,t=%4d] state=%4s / action=%s / reward=%7.4f / next_state=%4s / Q[s]=%s" % (episode, t, state, action, reward, next_state, agent.q_model.predict_one(state)))
-        if episode % 100 == 0 or episode > 3000:
-            env.render()
-            time.sleep(0.01)
-
-        if done:
-            break
-        state = next_state
-
-    # 에피소드가 끝날 때마다 Q-network 을 학습하고, epsilon 을 점차 낮춘다.
-    agent.train()
-    agent.update_epsilon()
-
-    # 에피소드마다 10번마다 target network 의 파라미터를 현재 Q-network 파라미터로 갱신해준다.
-    if episode % 10 == 0:
-        agent.update_target_q_weights()
-
-    print('[%4d] Episode reward=%.4f / epsilon=%f' % (episode, episode_reward, agent.epsilon))
-time.sleep(10)
+agent = DQN(env, discount=0.99)
+agent.train()
