c19d22b3531aa6bae9f96055463bf96c186dedef
Jaeyoung Park <jypark@MacBook-Pro-2.local>
2020-07-23 22:37:24 +0900

    fix install scripts

---
+++ env_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conda env create -f environment.yml
+
+source ~/Programs/anaconda3/etc/profile.d/conda.sh
+conda activate drl
+
+rm -rf tmp && mkdir -p tmp && cd tmp && git clone https://github.com/benelot/pybullet-gym.git && cd pybullet-gym && pip install -e .
